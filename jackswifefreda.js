0f90e9fbb0600360e06ce71a5e460d386cd77e1a
Yuka Nagai <yukanagai@gmail.com>
2015-08-26 08:53:42 -0400

    testing

---
+++ jackswifefreda.js
@@ -0,0 +1,89 @@
+$(function() {
+
+
+var menu = {
+	breakfast: [
+		{name: "Soft Boiled Eggs with Sliders", price: 7},
+		{name: "Grapefruit and Yogurt", price: 8},
+		{name: "Rosewater Waffle", price: 10},
+		{name: "Madame Freda", price: 13},
+		{name: "Green Shakshuka", price: 12},
+		{name: "Mediterranean Breakfast", price: 13},
+		{name: "Poached Eggs with Grilled Tomato and Haloumi", price: 12},
+		{name: "jack's breakfast", price: 19},
+		{name: "Toasted Baguette", price: 5}
+	],
+
+	sides: [
+		{name: "Couscous", price: 6},
+		{name: "French Fries", price: 6},
+		{name: "Chopped Salad", price: 6},
+		{name: "Arugula Salad", price: 6}
+	],
+
+	salads:[
+		{name: "Greek Salad", price: 12},
+		{name: "Tuna Salad", price: 14}
+	],
+
+	entrees: [
+		{name: "Chicken Kebab", price: 13},
+		{name: "Fish a la Plancha", price: 17},
+		{name: "Mustard Seed Crusted Tofu", price: 13},
+		{name: "Freda's Matzo Ball Soup", price: }
+	],
+
+	sandwiches: [
+		{name: "Jack's Burger", price: 13},
+		{name: "Prego Roll", price: 14},
+		{name: "Chicken Prego", price: 13},
+		{name: "Grilled Eggplant Baguette", price: 10},
+		{name: "Classic Egg Salad Sandwich", price: 9},
+		{name: "Mashed Avocado on Seeded Bread", price: 11}
+	],
+
+	dessert: [
+		{name: "Flourless Chipotle Chocolate Cake", price: 7},
+		{name: "Baked Fruit Crips", price: 7},
+		{name: "Homemade Spiced Cheesecake", price: 7},
+		{name: "Malva Pudding", price: 7}
+	],
+
+	drinks: [
+		{name: "Mint Lemonade", price: 5},
+		{name: "Fresh Cantaloupe Juice", price: 5},
+		{name: "Bottomless Drip Coffee", price: 3},
+		{name: "Espresso", price: 3},
+		{name: "Au Lait", price: 3.5},
+		{name: "Latte", price: 4},
+		{name: "Cappucino", price: 4},
+		{name: "Cold Brew", price: 3.5},
+		{name: "Nana Tea", price: 4}
+	]
+
+	alcohol: [
+		{name: "Fleur du Chenin Blanc", price: 8},
+		{name: "Schlumberger Pinot Blanc", price: 9},
+		{name: "Lauren Miquel Rose’", price: 9},
+		{name: "Alta Vista Malbec", price: 8},
+		{name: "Delas Freres Cotes du Rhone", price: 9},
+		{name: "Sparkling Wine: Segura Viudas Brut Reserva Cava", price: 9},
+		{name: "Stella Artois", price: 4},
+		{name: "Goose Island IPA", price: 4},
+		{name: "Boddington in Can", price: 6},
+		{name: "Hoegaarden", price: 4},
+		{name: "Guinness", price: 5},
+		{name: "Bootsy Collins", price: 12},
+		{name: "Pimm's Cup No.1", price: 12},
+		{name: "Cantaloupe Mimosa", price:10},
+		{name: "Bloody Mary", price: 10},
+	]
+
+};
+
+
+
+
+
+
+})
